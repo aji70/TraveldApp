0a7e73e9365af263eb2ec0784bb67865fb68197f
Aji <realjaiboi70@gmail.com>
2024-05-14 14:44:45 +0100

    update

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,36 +1,19 @@
 import { useState } from "react";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Shorts", quantity: 5, packed: true },
+];
 export default function App() {
-  const [items, setItems] = useState([]); //This state manipulates the array of items added to the array
-  function handleAddItems(item) {
-    // we are not allowed to mutate state directly in react so we create a new array by,
-    // spreading the former array and adding the new item to it all in a new array
-    setItems((items) => [...items, item]);
-  }
-
-  function handleDeleteItems(id) {
-    setItems((items) => items.filter((item) => item.id !== id));
-  }
-
-  function handleToggleItem(id) {
-    setItems((items) =>
-      items.map((item) =>
-        item.id === id ? { ...item, packed: !item.packed } : item
-      )
-    );
-  }
-
   return (
     <div className="app">
       <Logo />
-      {/* passing a function as a props so it can be used in a component */}
-      <Form onAddItems={handleAddItems} />
-      {/*Passing the items as a props to the PackingList component*/}
-      <PackingList
-        items={items}
-        onDeleteItems={handleDeleteItems}
-        onToggleItems={handleToggleItem}
-      />
+
+      <Form />
+
+      <PackingList />
+
       <Stats />
     </div>
   );
@@ -39,43 +22,49 @@ export default function App() {
 function Logo() {
   return <h1>Far away</h1>;
 }
-function Form({ onAddItems }) {
-  //Accepting the function that was passed in as a prop
+
+function Form() {
   const [description, setDescription] = useState(" ");
   const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
-    // Guard Clause to prevent submission with empty field
-    if (!description) return;
-    const newItem = { description, quantity, packed: false, id: Date.now() };
 
-    onAddItems(newItem); //Finally adding the element to the new array
+    if (!description) return; //Guard clause that does not allow submission if the description is not filled
+    const newItem = { description, quantity, packed: false, oid: Date.now() };
+    console.log(newItem);
 
-    // set everything back to it's normal state
-    setDescription("");
+    setDescription(" ");
     setQuantity(1);
-    // console.log(newItem);
   }
+
   return (
     <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your trip</h3>
-
+      <h3>What do you need for your trip?</h3>
       <select
         value={quantity}
         onChange={(e) => setQuantity(Number(e.target.value))}
       >
-        {/* An array of numbers 1 - 20  and mapping the numbers to form the options including their value and 
-        the option*/}
+        {/*
+        e.target.value is always a string
+        <option value="1">1</option>
+        <option value="2">2</option> 
+        this is done more simply below
+        an array of 20 elements
+        we then loop over the array and use the elements 
+        as the value of the select
+        num is the value of every element in the array from 0 - 20
+        */}
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
+
       <input
         type="text"
-        placeholder="Item...."
+        placeholder="Items..."
         value={description}
         onChange={(e) => setDescription(e.target.value)}
       />
@@ -83,50 +72,36 @@ function Form({ onAddItems }) {
     </form>
   );
 }
-function PackingList({ items, onDeleteItems, onToggleItems }) {
-  //Accepting the item array in the packing list component
+
+function PackingList() {
   return (
     <div className="list">
       <ul>
-        {items.map(
-          (
-            item // items is the array defined up in the app component
-          ) => (
-            <Item
-              onDeleteItems={onDeleteItems}
-              item={item}
-              key={item.id}
-              onToggleItems={onToggleItems}
-            /> //Passing the function that hhelps deletes items from the array as a prop
-          )
-        )}
+        {/* We enter JavaScript mode by using the { }, we the map the elements in the array to an item component so each of them will be represented as a component and pass the whole element as a prop to the component*/}
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
       </ul>
     </div>
   );
 }
-function Item({ item, onDeleteItems, onToggleItems }) {
-  //Accepting the function that hhelps deletes items from the array as a prop
+
+function Item({ item }) {
   return (
     <li>
-      <input
-        type="checkbox"
-        value={item.packed}
-        onChange={() => onToggleItems(item.id)}
-      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button onClick={() => onDeleteItems(item.id)}>❌</button>
-      {/* getting the id of the particular item we want to delete */}
+
+      <button>❌</button>
     </li>
   );
 }
+
 function Stats() {
   return (
     <footer className="stats">
-      <em>
-        you have X items on your list, and you have already packed X(X%) items
-      </em>
+      <em>You have X items and you have already packed X(X%)</em>
     </footer>
   );
 }
